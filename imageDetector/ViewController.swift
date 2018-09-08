27dde64f678441c1e8f7f77b5b4d09668133e7ca
Maxwell <maxwell.s.school@gmail.com>
2018-09-07 22:08:54 -0400

    Initial Commit

---
+++ imageDetector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  imageDetector
+//
+//  Created by Maxwell Stone on 9/7/18.
+//  Copyright © 2018 Maxwell Stone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
